2bb7eba6a9f1a4ec5d5cc5aa3da5b45a4b25afb5
FalloutX <inderjitsidhu@outlook.com>
2013-08-24 15:32:05 +0530

    4 Static Pages Added into the app.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,75 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', 'Sample App')                          
       
     end
+
+    it "should have indicative title" do
+
+      visit '/static_pages/home'
+      page.should have_title("Rails Sample App | Home")
+      
+    end
+
+
+  end
+
+  describe "Help page" do
+    
+    it "should have the h1 'Help'" do
+      
+      visit '/static_pages/help'
+      page.should have_selector('h1',' Help')
+      
+    end
+
+    it "should have indicative title" do
+
+      visit '/static_pages/help'
+      page.should have_title('Rails Sample App | Help')
+      
+    end
+  
+  end
+
+  describe "About page" do
+    
+    it "should have the h1 'About Us'" do
+      
+      visit '/static_pages/about'
+      page.should have_selector('h1', 'About Us')
+      
+    end
+
+    it "should have indicative title" do
+
+      visit '/static_pages/about'
+      page.should have_title("Rails Sample App | About Us")
+      
+    end
+
+  end
+
+  describe "Contact page" do
+    
+    it "should have the  h1 'Contact'" do
+      
+      visit '/static_pages/contact'
+      page.should have_selector('h1', 'Contact')                          
+      
+    end
+
+    it "should have indicative title" do
+
+      visit '/static_pages/contact'
+      page.should have_title("Rails Sample App | Contact")
+      
+    end
+
+
   end
 end
