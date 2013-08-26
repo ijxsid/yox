6e80f226c75b7ae8d0c4f0338a8a89f2fc22731f
FalloutX <inderjitsidhu@outlook.com>
2013-08-26 12:56:51 +0530

    Signup page functionality enhanced.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,4 +17,38 @@ describe "UserPages" do
     it { should have_title(full_title(user.name)) }
       
   end
+
+  describe "Signup" do
+    before { visit signup_path }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button "Create my account" }.not_to change(User, :count)
+        
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "ijs@oaks.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      it "should create a user" do
+        expect { click_button "Create my account" }.to change(User, :count).by(1)
+      end
+
+      describe "after saving a user" do
+        before { click_button "Create my account"}
+        let(:user) { User.where(email: "ijs@oaks.com").take }
+
+        it { should have_title(user.name) }
+      end
+
+    end
+
+
+
+  end
 end
