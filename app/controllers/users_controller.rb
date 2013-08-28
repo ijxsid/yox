a6a36797fdaf3e1a71b5956ab9b12095addbc254
FalloutX <inderjitsidhu@outlook.com>
2013-08-28 11:50:42 +0530

    Fixing the Signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "You are Welcome, #{@user.name}"
       
       redirect_to @user
