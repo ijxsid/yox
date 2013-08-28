9b51520f4c33984a39c0e8d0b1c7384a7be8904c
FalloutX <inderjitsidhu@outlook.com>
2013-08-28 14:23:52 +0530

    Adding an Edit profile page.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -62,4 +62,24 @@ describe "UserPages" do
 
 
   end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+
+    describe "page" do
+    
+    it { should have_selector('h1', text: "Update your profile") }
+    it { should have_title("Edit user") }
+    it { should have_link("change", href: "https://gravatar.com/emails")}  
+    end
+    
+    describe "with invalid information" do
+      before { click_button "Save changes"}
+      
+      it { should have_content('error') }
+      
+    end
+  end
 end
