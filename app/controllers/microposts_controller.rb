6baa0e21260ca8d818313708d404c875316df312
FalloutX <inderjitsidhu@outlook.com>
2013-09-03 12:34:25 +0530

    Adding deletion of microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,12 +1,12 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
-
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created successfully!"
-      redirect_to root_url
+      flash[:success] = "Ahoy, Micropost created successfully!"
+      redirect_to root_path
     else
       @feed_items = []
       render 'static_pages/home'
@@ -15,11 +15,19 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    Micropost.destroy(@micropost)
+    flash[:success] = "Micropost deleted successfully!"
+    redirect_to root_path
   end
 
   private
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.where(id: params[:id])
+       redirect_to root_path if @micropost.nil?
     end
 
 
--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -29,9 +29,20 @@ describe "MicropostPages" do
       it "should not create a micropost" do
         expect {click_button "Post"}.to change(Micropost, :count).by(1)
       end
-      
+
     end
     end
   end
 
+  describe "micropost destruction " do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as a correct user " do
+      before { visit root_path}
+
+      it "should delete a micropost " do
+        expect { click_link "delete"}.to change(Micropost, :count).by(-1)
+      end
+    end
+  end
 end
